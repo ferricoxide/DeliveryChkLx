5fe84106ebd042c9a0b54fb51608be765637e820
Thomas H Jones II <github@xanthia.com>
2015-07-01 15:18:48 +0000

    Create roll-back

--- GrubCfg.sh
+++ GrubCfg.sh
@@ -3,6 +3,7 @@
 # Script to ensure that GRUB-related config files exist in their proper form
 #
 ############################################################
+SAVDATE=$(date "+%Y%m%d%H%M")
 GRUBCFG="/boot/grub/grub.conf"
 GRUBETC="/etc/grub.conf"
 
@@ -15,6 +16,35 @@ then
       printf "${GRUBCFG} is empty: does this "  > /dev/stderr
       echo "system use GRUB to boot" > /dev/stderr
       RETCODE=1
+   fi
+
+   # Verify /etc/grub.conf state
+   if [ -e ${GRUBETC} ]
+   then
+      # See if /etc/grub.conf is a symlink
+      if [ -h ${GRUBETC} ]
+      then
+         WHEREPT=$(readlink ${GRUBETC})
+         # Ensure /etc/grub.conf points to /boot/grub/grub.conf
+         if [ "${WHEREPT}" != "${GRUBCFG}" ]
+         then
+            printf "${GRUBETC} does not point to " > /dev/stderr
+            echo "${GRUBCFG}. Fixing." > /dev/stderr
+            # Let's save the contents for later...
+            mv ${GRUBETC} ${GRUBETC}-BAK_${SAVDATE}
+            if [[ $? -eq 0 ]]
+            then
+               ln -s ${GRUBCFG} ${GRUBETC}
+            else
+               echo "Cannot fix ${GRUBETC}"
+               RETCODE=1
+            fi
+            RETCODE=1
+         fi
+      fi
+##    else
+##       ln -s ${GRUBCFG} ${GRUBETC} || { RETCODE=1 ; \
+##         echo "Link-creation failed" > /dev/stderr }
    fi
 else
    # Alert if file does not exist
