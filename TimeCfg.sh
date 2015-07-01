196dc5c0546d7d1949b71ad40067d41a52bba52b
Thomas H Jones II <github@xanthia.com>
2015-07-01 18:19:12 +0000

    Roll-back checkpoint

--- TimeCfg.sh
+++ TimeCfg.sh
@@ -8,11 +8,24 @@
 ############################################################
 WANTEDTZ=${1:-UTC}
 ZONEFILDIR="/usr/share/zoneinfo"
+CLOCKCFG="/etc/sysconfig/clock"
 
+# Ensure valid TZ is specified
 if [ -f ${ZONEFILDIR}/${WANTEDTZ} ]
 then
    echo "Using \"${WANTEDTZ}\" for validity-testing."
 else
    echo "Invalid timezone [${WANTEDTZ}] specified. Aborting." > /dev/stderr
    exit 1
+fi
+
+SYSCFGVAL=$(awk -F "=" '/'${WANTEDTZ}'/{ print $2 }' ${CLOCKCFG} |
+   grep -x ${WANTEDTZ})
+
+if [ "${SYSCFGVAL}" = "" ]
+then
+   echo "${WANTEDTZ} not set in ${CLOCKCFG}." > /dev/stderr
+   RETCODE=1
+else
+   echo "${WANTEDTZ} set in ${CLOCKCFG}."
 fi
