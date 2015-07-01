fe874336f6290ac7b7df5f88323e447cc5e11c99
Thomas H Jones II <github@xanthia.com>
2015-07-01 18:00:23 +0000

    Initial check-in

---
+++ TimeCfg.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Script to ensure that host's timzeone configuration is
+# correctly set up. Invoke with optional timezone argument:
+#
+# Usage: TimeCfg.sh [ TIMEZONE ]
+#
+############################################################
+WANTEDTZ=${1:-UTC}
+ZONEFILDIR="/usr/share/zoneinfo"
+
+if [ -f ${ZONEFILDIR}/${WANTEDTZ} ]
+then
+   echo "Using \"${WANTEDTZ}\" for validity-testing."
+else
+   echo "Invalid timezone [${WANTEDTZ}] specified. Aborting." > /dev/stderr
+   exit 1
+fi
