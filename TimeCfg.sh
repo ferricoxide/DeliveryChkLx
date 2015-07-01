7ecb6f3a43a0a33921b099c13021265c5cdfa8de
Thomas H Jones II <github@xanthia.com>
2015-07-01 16:57:41 -0400

    Roll-back checkpoint

--- TimeCfg.sh
+++ TimeCfg.sh
@@ -9,6 +9,7 @@
 WANTEDTZ=${1:-UTC}
 ZONEFILDIR="/usr/share/zoneinfo"
 CLOCKCFG="/etc/sysconfig/clock"
+LOCALTIME="/etc/localtime"
 
 # Ensure valid TZ is specified
 if [ -f ${ZONEFILDIR}/${WANTEDTZ} ]
@@ -19,18 +20,19 @@ else
    exit 1
 fi
 
+# Verify TZ value in /etc/sysconfig/clock
 if [ -f ${CLOCKCFG} ]
 then
    # Compare desired TZ value to one set in /etc/sysconfig/clock
-   SYSCFGVAL=$(awk -F "=" '/'${WANTEDTZ}'/{ print $2 }' ${CLOCKCFG} |
-      grep -x ${WANTEDTZ})
+   SYSCFGVAL=$(awk -F "=" '/'$(echo ${WANTEDTZ} | \
+     sed 's#/#\\/#g')'/{ print $2 }' ${CLOCKCFG} | grep -x ${WANTEDTZ})
    
    # Fix /etc/sysconfig/clock if necessary
    if [ "${SYSCFGVAL}" = "" ]
    then
       printf "${WANTEDTZ} not set in ${CLOCKCFG}. " > /dev/stderr
       echo "Attempting to fix." > /dev/stderr
-      sed -i '/^ZONE/s/=.*/='${WANTEDTZ}'/' ${CLOCKCFG}
+      sed -i '/^ZONE/s#=.*#='${WANTEDTZ}'#' ${CLOCKCFG}
       if [[ $? -eq 0 ]]
       then
          echo "Updated 'ZONE' setting in ${CLOCKCFG} to ${WANTEDTZ}."
@@ -40,5 +42,22 @@ then
       fi
    else
       echo "${WANTEDTZ} already set in ${CLOCKCFG}."
+   fi
+else
+   echo "ZONE=${WANTEDTZ}" >> ${CLOCKCFG} && echo "Created ${CLOCKCFG}"
+fi
+
+# Verify that /etc/localtime is correct
+if [ -f ${LOCALTIME} ]
+then
+   echo "${LOCALTIME} exists"
+else
+   cp "${ZONEFILDIR}/${WANTEDTZ}" "${LOCALTIME}"
+   if [[ $? -eq 0 ]]
+   then
+      echo "Created ${LOCALTIME}"
+   else
+      echo "Creation of ${LOCALTIME} failed." > /dev/stderr
+      RETCODE=1
    fi
 fi
