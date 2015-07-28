090bee6c51d094ce0aa658192d7302a24fe6192b
Thomas H Jones II <github@xanthia.com>
2015-07-28 22:53:14 +0000

    Added fix-option

--- GrubCfg.sh
+++ GrubCfg.sh
@@ -7,9 +7,21 @@
 GRUBCANON="/boot/grub/grub.conf"
 GRUBFILES=($(echo /etc/grub.conf ;find /boot -name grub.conf -o -name menu.lst))
 GRUBCFIND=$(stat -c %i ${GRUBCANON})
+FIX=${FIX:-0}
 
 TOKERR="\033[0;33m[CHECK]\033[0m"
 TOKAOK="\033[0;32m[OK]\033[0m"
+
+# Fix borked up files
+function FixLink() {
+   local TARGET="${1}"
+   rm -f "${TARGET}"
+   ln "${GRUBCANON}" "${TARGET}" > /dev/null 2>&1
+   if [[ $? -ne 0 ]]
+   then
+      ln -s "${GRUBCANON}" "${TARGET}"
+   fi
+}
 
 while [[ COUNT -lt ${#GRUBFILES[@]} ]]
 do
@@ -30,9 +42,12 @@ do
        printf "${TOKAOK}\t${GRUBFILES[${COUNT}]} is a sym-link to ${CHKLINK}\n"
    else
        printf "${TOKERR}\t${GRUBFILES[${COUNT}]} is a sym-link to ${CHKLINK}\n"
+       if [[ ${FIX} -eq 1 ]]
+       then
+          FixLink ${GRUBFILES[${COUNT}]}
+       fi
    fi
 
    # Up our counter
    ((COUNT++))
 done
-
