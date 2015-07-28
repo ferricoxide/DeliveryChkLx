c2889180e399097da706986e4165f241fd6953de
Thomas H Jones II <github@xanthia.com>
2015-07-28 22:40:05 +0000

    Report logic in place

--- GrubCfg.sh
+++ GrubCfg.sh
@@ -8,24 +8,31 @@ GRUBCANON="/boot/grub/grub.conf"
 GRUBFILES=($(echo /etc/grub.conf ;find /boot -name grub.conf -o -name menu.lst))
 GRUBCFIND=$(stat -c %i ${GRUBCANON})
 
+TOKERR="\033[0;33m[CHECK]\033[0m"
+TOKAOK="\033[0;32m[OK]\033[0m"
+
 while [[ COUNT -lt ${#GRUBFILES[@]} ]]
 do
+   # Determine if real file-content location
    CHKLINK=$(readlink -f ${GRUBFILES[${COUNT}]})
 
    if [[ "${CHKLINK}" = "${GRUBFILES[${COUNT}]}" ]]
    then
       if [[ "${CHKLINK}" = "${GRUBCANON}" ]]
       then
-         echo "This is the canonical grub.conf"
-      else
-         echo "This is NOT the canonical grub.conf"
+         printf "${TOKAOK}\t${GRUBFILES[${COUNT}]} is the canonical grub.conf\n"
+      elif [[ $(stat -c %i ${GRUBFILES[${COUNT}]}) -eq ${GRUBCFIND} ]]
+      then
+         printf "${TOKAOK}\t${GRUBFILES[${COUNT}]} is a hardlink to ${GRUBCANON}\n"
       fi
+   elif [[ "${CHKLINK}" = "${GRUBCANON}" ]]
+   then
+       printf "${TOKAOK}\t${GRUBFILES[${COUNT}]} is a sym-link to ${CHKLINK}\n"
    else
-       echo "${GRUBFILES[${COUNT}]} is a sym-link to ${CHKLINK}"
+       printf "${TOKERR}\t${GRUBFILES[${COUNT}]} is a sym-link to ${CHKLINK}\n"
    fi
 
    # Up our counter
    ((COUNT++))
 done
 
-echo ${GRUBFILES[@]}
