24949e5760fe00e7a93a6d3634bce25bd7688196
Thomas H Jones II <github@xanthia.com>
2015-07-29 21:27:24 +0000

    Added link check/fix logic

--- SELchk.sh
+++ SELchk.sh
@@ -9,3 +9,27 @@
 FIX=${FIX:-0}
 SELCFCANON=/etc/selinux/config
 SELCFSYSCF=/etc/sysconfig/selinux	# Linkpath ../selinux/config
+
+# Color-coded output tags
+TOKERR="\033[0;33m[CHECK]\033[0m"
+TOKAOK="\033[0;32m[OK]\033[0m"
+TOKINF="\033[0;0m[INFO]\033[0m"
+
+function ChkSELlink() {
+   if [[ $(readlink -f ${SELCFSYSCF}) = ${SELCFCANON} ]]
+   then
+      printf "${TOKAOK}\t${SELCFSYSCF} is a symlink to ${SELCFCANON}\n"
+   else
+      printf "${TOKERR}\t${SELCFSYSCF} is NOT a symlink to ${SELCFCANON}"
+      if [[ ${FIX} -eq 1 ]]
+      then
+         echo " ...fixing."
+         rm ${SELCFSYSCF}
+         (cd /etc/sysconfig ; ln -s ../selinux/config ${SELCFSYSCF})
+      else
+         echo
+      fi
+   fi
+}
+
+ChkSELlink
