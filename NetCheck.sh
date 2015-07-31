921adce23519b38ac8ba10c61c6f7d76b6c72803
Thomas H Jones II <github@xanthia.com>
2015-07-31 14:44:17 +0000

    Adding xinetd checks

--- NetCheck.sh
+++ NetCheck.sh
@@ -13,3 +13,27 @@
 #   ( ) Feature not implemented
 #
 #################################################################
+HAVEXINETD=$(rpm --quiet -q xinetd && echo "yes" || echo "no")
+
+# Color-coded status tokens
+TOKERR="\033[0;33m[CHECK]\033[0m"
+TOKAOK="\033[0;32m[OK]\033[0m"
+TOKINF="\033[0;0m[INFO]\033[0m"
+
+function CheckXinetdStuff() {
+   service xinetd status > /dev/null 2>&1
+   if [[ $? -eq 0 ]]
+   then
+      printf "${TOKINF}\tXinetd service-launcher running.\n"
+   else
+      printf "${TOKINF}\tXinetd service-launcher not running.\n"
+   fi 
+}
+
+if [[ "${HAVEXINETD}" = "yes" ]]
+then
+   printf "${TOKINF}\tXinetd service-launcher installed.\n"
+   CheckXinetdStuff
+else
+   printf "${TOKINF}\tXinetd service-launcher not installed.\n"
+fi
