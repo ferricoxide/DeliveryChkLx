a4d43e80bbf502bcdc437f4a6bfea2356c5da911
Thomas H Jones II <github@xanthia.com>
2015-07-31 13:59:07 +0000

    Initial check-in

---
+++ NetCheck.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# * Network Services
+#   * Verify that nameservers defined in /etc/resolv.conf are reachable	( )
+#   * Verify that ntp servers defined in /etc/ntp.conf are reachable	( )
+#   * Check configuration of IPTables					( )
+#   * Check configuration of /etc/hosts.allow				( )
+#   * Check configuration of /etc/hosts.deny				( )
+#   * Check configuration of xinetd					( )
+#
+# LEGEND:
+#   (✓) Feature implemented
+#   ( ) Feature not implemented
+#
+#################################################################
